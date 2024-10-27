04d168e6eb98737fbbb91d09903c9c307e035e44
= <elkhalfaouizakaria@gmail.com>
2024-10-27 17:11:31 +0100

    java 2

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
+class Student {
+    int Id;
+    String Name;
+    public Student(int id, String name) {
+        this.Id = id;
+        this.Name = name;
+    }
+}
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         
+        Student[] students = new Student[2];
+
+        students[0] = new Student(0, "Zakaria");
+        students[1] = new Student(1, "Sara");
+
+        for(Student student: students) {
+            System.out.println(student.Id + " | " + student.Name);
+        }
+        
+        String name = "Sara";
+        String str = new String("Zakaria");
+
+        StringBuffer strB = new StringBuffer("Ommi"); // StringBuffer is thread safe
+        strB.append(" is thd best Mother");
+        System.out.println(strB);
+        
+        StringBuilder st = new StringBuilder("Hala Madrid y Nada Màs"); // String builder is not thread safe
+        // String strin = st.toString();
+        System.out.println(st);
+
     }
 }
