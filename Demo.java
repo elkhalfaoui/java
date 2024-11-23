bf6309c4084c2eec432868c6cf9de94e8d48c106
= <elkhalfaouizakaria@gmail.com>
2024-11-23 17:48:48 +0100

    java 1

--- Demo.java
+++ Demo.java
@@ -20,13 +20,45 @@ class Calculator {
 public class Demo {
     public static void main(String args[]) {
 
-        int obj1 = new Calculator().add(4, 4);
+        Calculator obj1 = new Calculator();
 
-        // int a = obj1.add(176, 17);
+        int a = obj1.add(176, 17);
         
-        System.out.println(obj1);
+        System.out.println(a);
 
         Calculator.addition(1, 3);
-        
+
+        int[] arr = new int[2];
+        System.out.println(arr[0]);
+        int[] nums = {1,2,3};
+        for(int i:nums) {
+
+            System.out.println(i);
+        }
+
+        int multi [][] = new int[3][3];
+        for(int one[]:multi) {
+            for(int two:one) {
+                System.out.print(two + " ");
+            }
+            System.out.println();
+        }
+
+        int jagged [][] = new int[3][];
+        jagged[0] = new int[5];
+        jagged[1] = new int[9];
+        jagged[2] = new int[1];
+
+        for(int one[]:jagged) {
+            for(int two:one) {
+                two = 1; //! it will not change the original array
+                System.out.print(two + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(jagged[0][0]);
+
+        Calculator objArr[] = new Calculator[1];
+        objArr[0] = obj1;
     }
 }
